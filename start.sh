dfa31e15150952383ff88f2312649d8c6f9b4917
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-08-15 16:22:45 +0300

    initial commit

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
- export MOVIE_API_KEY='4040e54d386115daa5e13290ddf271de'
+export MOVIE_API_KEY='4040e54d386115daa5e13290ddf271de'
 export SECRET_KEY='1234!'
 
-# python3.6 manage.py server
+export MAIL_USERNAME='michael.m.njoroge254@gmail.com'
+export MAIL_PASSWORD='Michienza254'
+
+python3.9 manage.py server
